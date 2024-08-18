1f916119c266fe1f1fcb057173ce83c4a89fc767
Mario Morales <mariomoraless@gmail.com>
2024-08-18 17:26:04 -0400

    Actualizacion JS y CSS

--- script.js
+++ script.js
@@ -11,37 +11,44 @@ const traductor = document.querySelector(".traductor");
 const elemento = document.querySelector(".contenedor_dos_mensaje");
 const validacion = document.querySelector(".mensaje_validacion");
 
-function mostrarBoton(){
-    let btn = document.querySelector(".btn_copiar");
-    btn.style.visibility = "inherit";
-
-}
 function btnEncriptar(){
+    let texto = textArea.value
+    let textoValidado = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, " ") ;
 
-    validarTexto()
-    const txtEncriptado = encriptar(textArea.value);
+    if(texto ==""){
+        validacion.style.fontWeight = "700";
+        validacion.textContent = "Ingrese un texto para encriptar";
+        setTimeout(()=>{
+            validacion.removeAttribute("style")
+        },1500);
+        
+        
+    }
+    else if (texto !== textoValidado){
+        validacion.style.fontWeight = "700";
+        validacion.textContent = "Texto no debe caracteres especiales";
+        setTimeout(()=>{
+            validacion.removeAttribute("style")
+        },1500);
+        
+    }
+            else{
+        const txtEncriptado = encriptar(textArea.value);
+        traductor.value = txtEncriptado;
+        textArea.value = "";
+        elemento.style.visibility = "hidden";
+        mostrarBoton();
+   }
+}
+
+function btnDesencriptar(){
+    const txtEncriptado = desencriptar(textArea.value);
     traductor.value = txtEncriptado;
     textArea.value = "";
     elemento.style.visibility = "hidden";
     mostrarBoton();
-     
-}
-
-// Validaciones
-
-function validarTexto(){
-    let text = textArea.value
-    let textValidado = text.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f'] /g,"" ) ;
-
-    if(text==""){
-         validacion.textContent = "Ingrese un texto para encriptar";
-         elemento.style.visibility = "hidden"
-
-
-    }
-}
-
-
+    setInterval("location.reload()",4000);
+};
 
 function encriptar(textoEncriptado){
     let matrizCodigo =[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
@@ -53,6 +60,30 @@ function encriptar(textoEncriptado){
         }
     }
     return textoEncriptado;
-}
+};
 
- 
+
+function desencriptar(textoDesencriptado){
+    let matrizCodigo =[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    textoDesencriptado = textoDesencriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(textoDesencriptado.includes(matrizCodigo[i][1])){
+            textoDesencriptado = textoDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return textoDesencriptado;
+};
+
+function btnCopiar(){
+    let copiar = traductor.value;
+    navigator.clipboard.writeText(copiar);
+    
+    setInterval("location.reload()",1500);
+};
+
+function mostrarBoton(){
+    let btn = document.querySelector(".btn_copiar");
+    btn.style.visibility = "inherit";
+
+};
